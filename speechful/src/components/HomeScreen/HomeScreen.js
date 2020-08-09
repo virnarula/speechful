0cef66bd1b4e98c580839986b936d28d51f23e2b
Vir Narula <virsvirus@gmail.com>
2020-08-09 00:26:10 -0500

    Create skeleton functions for HomeScreen

--- speechful/src/components/HomeScreen/HomeScreen.js
+++ speechful/src/components/HomeScreen/HomeScreen.js
@@ -1,10 +1,42 @@
 import React from 'react'
+import {Grid, Paper} from '@material-ui/core/'
 import './HomeScreen.css'
+import Document from '../../model/Document'
+
+class DocumentItem extends React.Component {
+    constructor(document) {
+        this.document = document;
+    }
+    render() {
+
+        return (
+            <Paper>
+
+            </Paper>
+        );
+    }
+}
+
+function getDocumentList() {
+    return [];
+}
+
+function getDocumentItems(documentList) {
+    return (
+    <Grid>
+        <DocumentItem>
+
+        </DocumentItem>
+    </Grid>
+    );
+}
 
 function HomeScreen () {
+    var documents = getDocumentList();
     return (
         <div className="HomeScreen">
             <h1> Documents </h1>
+            getDocumentItems(documents);
         </div>
     );
 }
