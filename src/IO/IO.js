8b791f21bb6eb6869274a1c8afbcec70500ff9e1
Vir Narula <virsvirus@gmail.com>
2020-08-11 02:16:16 -0500

    Implement IO Functions

--- src/IO/IO.js
+++ src/IO/IO.js
@@ -1,11 +1,40 @@
-function readDocumentList () {
-    return [];
+import { readFile, readdir, exists, writeFile } from 'fs'
+
+function readDocumentFromFile(id) {
+    let filePath = "../data/" + id.toString() + ".json";
+    let file;
+    if (!exists(filePath)) {
+        throw "File with id [" + id.toString() + "] not found"
+    }
+    file = JSON.parse(readFile(filePath))
+    return file;
 }
 
-function readDocumentFromFile() {
-    
+function getDocumentIds() {
+    let filePath = "../data/";
+    readdir(filePath, (err, files) => {
+        if(err) {
+            throw "Error reading file path"
+        } else {
+            return files;
+        }
+    })
 }
 
-function writeDocumentToFile () {
+function saveExistingDocument(document) {
+    let filePath = "../data/" + document.id.toString + ".json";
+    writeFile(filePath, JSON.stringify(document), (err) => {
+        if (err){
+            throw "Error writing file";
+        }
+    });
+}
 
+function saveNewDocument(document) {
+    document.id = getNextId();
+    saveExistingDocument(document);
+}
+
+function getNextId(){
+    return getDocumentIds().size() + 1;
 }
