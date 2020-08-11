044b3556ac579b847bdd27a02717a84241ae3fff
Vir Narula <virsvirus@gmail.com>
2020-08-11 18:32:33 -0500

    Add logo to startscreen

--- src/components/StartPage/StartPage.js
+++ src/components/StartPage/StartPage.js
@@ -1,17 +1,24 @@
 import React from 'react'
 import Button from '@material-ui/core/Button'
+import logo from "../../res/logo.png"
 import './StartPage.css'
 import StartDictaphone from '../../speech/StartDictaphone'
 
-function StartPage () {
-    return (
+function StartPage() {
+  return (
     <div className="StartPage">
       <header className="App-header">
-        <h3> Say "Hello" to get started!</h3>
+        <h3> Say "Hello"</h3>
           <StartDictaphone />
       </header>
+      <Button
+        variant='contained'
+        color="primary"
+        href="/home">
+        Or click here to get started
+      </Button>
     </div>
-    );
+  );
 }
 
 export default StartPage;
