c1cd3934ed3e6d020783d8870d47c2fa35178a39
vkrd <vduvvur2@illinois.edu>
2020-08-10 23:02:50 -0500

    Added dictaphone

--- src/components/HomeScreen/HomeScreen.js
+++ src/components/HomeScreen/HomeScreen.js
@@ -3,6 +3,7 @@ import {Grid, Paper, Button} from '@material-ui/core/'
 import './HomeScreen.css'
 import SearchBar from 'material-ui-search-bar'
 import Document from '../../model/Document'
+import Dictaphone from '../../speech/Dictaphone'
 
 function HomeScreen () {
     var documents = getDocumentList();
@@ -18,6 +19,7 @@ function HomeScreen () {
                 href="/document">
                 + Create New Document
             </Button>
+            <Dictaphone />
         </div>
     );
 }
