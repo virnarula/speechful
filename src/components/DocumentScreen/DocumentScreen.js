4879d31feead21c861240ab64cc545c9c9d3abad
Vir Narula <virsvirus@gmail.com>
2020-08-11 16:28:36 -0500

    Implement basic editor tools div

--- src/components/DocumentScreen/DocumentScreen.js
+++ src/components/DocumentScreen/DocumentScreen.js
@@ -2,6 +2,10 @@ import React from 'react'
 import json from '../../data/test2.json'
 import DocumentRender from '../Document/Document'
 import { makeDocument, updateParagraph } from '../../model/Document'
+import 'react-split-pane'
+import SplitPane from 'react-split-pane'
+import { Button } from '@material-ui/core'
+import './DocumentScreen.css'
 
 class DocumentScreen extends React.Component {
   constructor(props) {
@@ -10,7 +14,7 @@ class DocumentScreen extends React.Component {
       document: makeDocument(json.paragraphs, json.title)
     }
   }
-  
+
   paragraphChange = (i, p) => {
     this.setState({
       document: updateParagraph(this.state.document, i, p)
@@ -19,12 +23,41 @@ class DocumentScreen extends React.Component {
 
   render() {
     return (
-      <DocumentRender
-        document={this.state.document}
-        handleChange={this.paragraphChange}
-      />
+      <SplitPane split="vertical" defaultSize={250}>
+        <div class="tool-pane">
+          <Button
+            variant="contained"
+            color="primary"
+            href="/home">
+            Go home
+          </Button>
+
+          <h1 class="document-title"> {this.state.document.title}</h1>
+
+          <h1 class="document-title">Currently Viewing Full Document</h1>
+
+          <div class="circle">
+            <h1>Mic</h1>
+          </div>
+
+          <Button
+            variant="contained"
+            color="primary"
+            onClick={() => {
+              console.log("click");
+            }}>
+            Save file
+          </Button>
+        </div>
+        <div class="document-pane">
+          <DocumentRender
+            document={this.state.document}
+            handleChange={this.paragraphChange}
+          />
+        </div>
+      </SplitPane>
     )
-  }  
+  }
 }
 
 export default DocumentScreen;
